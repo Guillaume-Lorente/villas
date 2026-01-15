ab4d958019666e86e81cfc86203613fe022ed43a
Guillaume Lorente <guillaumelorente@orange.fr>
2026-01-15 18:19:29 +0100

    Add Google Tag Manager to layout.js

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -13,7 +13,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      {/* Google Tag Manager (prioritaire pour analytics/ads) */}
+      <Script
+        id="gtm"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T2VFWL7K');`,
+        }}
+      />
+      {/* End Google Tag Manager */}
+
       <body className="flex flex-col min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T2VFWL7K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Schema.org : Website */}
         <Script
           id="schema-website"
@@ -51,6 +76,7 @@ export default function RootLayout({ children }) {
           <main className="flex-grow">{children}</main>
           <Footer />
         </LanguageProvider>
+
         <Toaster position="top-center" />
       </body>
     </html>
